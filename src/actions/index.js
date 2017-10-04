a71c13f5a5d2e1bbb128844bbe5bff14523d7d90
jacob ruleaux <jakeruleaux@gmail.com>
2017-10-03 23:03:57 -0700

    add header to api key, some errors gone but no api response

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,9 +18,12 @@ export function getFlight(dispatch) {
     dispatch(requestFlight(flightId));
     console.log("start");
     console.log(headers);
-  
+
     return
-    fetch("https:www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDU0sK531yzL7Mu5NuLYYkorxExkmi_A44", headers).then(response => response.json(),
+    fetch("https:www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDU0sK531yzL7Mu5NuLYYkorxExkmi_A44", {
+      method: 'get',
+      headers: {'Content-Type' : 'application/json'}
+    }).then(response => response.json(),
     console.log("prebob"),
     error => console.log("error", error)
   ).then(function(json) {
